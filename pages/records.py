45d38f9a00a6ef5425f8266b05f09b4e67e252ab
sjitix <adrawing26@gmail.com>
2025-12-06 20:35:13 +0100

    add records

---
+++ pages/records.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Records", layout="centered")
+
+st.markdown("<h1 style='color: red;'>Records</h1>", unsafe_allow_html=True)
+
+# Add your records content here
+st.write("Your emotion records will appear here.")
